3bcb6f7cb14a64c6646866a31f0f7a54fd791d73
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-10-02 17:05:06 -0400

    blueCircles

--- challenge_4/sketch.js
+++ challenge_4/sketch.js
@@ -1,18 +1,25 @@
+// a simple p5 sketch that draws an ellipse
+var xPositions = [];
+var yPositions = [];
+var ballSizes = [];
+
 function setup() {
-	// create a place to draw
-	createCanvas(640, 360);
-	noStroke();
-	noLoop();
+	createCanvas(640, 480);
+	for (var i = 0; i < 10; i++){
+		xPositions[i] = random(0, 640);
+		yPositions[i] = random(0, 480);
+		ballSizes[i] = random(10, 30);
+	}
 }
 
 function draw() {
-	// clear the background
-	background(150, 50, 50);
+	background(125, 0, 0);
+	fill(0, 255, 255);
+	noStroke();
 
-	// set a fill color
-	fill(255, 255, 255);
-
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	for (var i = 0; i < 10; i++){
+		xPositions[i] = xPositions[i] + random(-2, 2);
+		yPositions[i] = yPositions[i] + random(-2, 2);
+		ellipse(xPositions[i], yPositions[i], ballSizes[i], ballSizes[i] );
+	}
 }
