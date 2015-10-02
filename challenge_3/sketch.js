c9b3982c6608737259f10f17737f8a45fc811a15
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-10-02 16:51:52 -0400

    lineX

--- challenge_3/sketch.js
+++ challenge_3/sketch.js
@@ -2,17 +2,32 @@ function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
 	noStroke();
-	noLoop();
+
 }
 
 function draw() {
 	// clear the background
-	background(150, 50, 150);
+	background(50, 50, 50);
 
 	// set a fill color
-	fill(255, 255, 255);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+	fill(255, 200, 100);
+	rect(50, 50, 200, 300);
+
+	stroke(255, 0, 0);
+	strokeWeight(30);
+	if (mouseX > 250) {
+		mouseX = 250;
+	}
+	if (mouseX < 50) {
+		mouseX = 50;
+	}
+	if (mouseY < 50) {
+		mouseY = 50;
+	}
+	if (mouseY > 350) {
+		mouseY = 350;
+	}
+	line(mouseX - 50, mouseY - 50, mouseX + 50, mouseY + 50);
+	line(mouseX - 50, mouseY + 50, mouseX + 50, mouseY - 50);
 }
