8c46a7b21e7e6fe5853703831bc9c60a132759b3
palat029 <TatyanaPalacios@tatyanas-mbp.wireless.newschool.edu>
2015-10-02 17:52:36 -0400

    pressHold

--- challenge_5/sketch.js
+++ challenge_5/sketch.js
@@ -1,18 +1,43 @@
+// remember where the ball is
+var ballX = 0;
+var ballY = 0;
+
 function setup() {
 	// create a place to draw
 	createCanvas(640, 360);
-	noStroke();
-	noLoop();
 }
 
 function draw() {
-	// clear the background
-	background(150, 150, 50);
+	/////// first way start
+	// // clear the background
+	// background(150, 150, 50);
+	// if (mouseIsPressed) {
+	// 	// down or clicked
+	// 	ellipse(ballX, ballY, 50, 50);
+	// } else {
+	// 	// up or not pressed
+	// 	ellipse(mouseX, mouseY, 50, 50);
+	// 	ballX = mouseX;
+	// 	ballY = mouseY;
+	// }
+	//////////// first way end
 
-	// set a fill color
+	//// second way start
+	background(250, 550, 110);
+	if (mouseIsPressed === false) {
+		ballX = mouseX;
+		ballY = mouseY;
+	}
+	stroke(255, 255, 255);
+	strokeWeight(8);
+	line(mouseX, mouseY, ballX, ballY);
+
 	fill(255, 255, 255);
+	stroke(0, 20, 250);
+	ellipse (ballX, ballY, 50, 50);
+	ellipse (mouseX, mouseY, 50, 50);
 
-	// draw the ellipse
-	var diameter = random(100, 200);
-	ellipse(320, 180, diameter, diameter);
+
+  //// second way end
+
 }
